5a80bbec4cd45889ad59390f60d4f9b1f825d5c6
Hugo Cezar <hugocssouza@gmail.com>
2024-02-24 19:05:10 -0400

    Create the mecanism to dynamic pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+  "id":1,
+  "photo":"https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q95/wp-content/uploads/2021/12/legiao_S26MdXFhkjiZ.jpg.webp",
+  "title": "Naruto comem lamem!",
+  "description": "E tava muito bom, tô certo!"
+  },
+  {
+    "id":2,
+    "photo":"https://criticalhits.com.br/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2021/11/Rock-Lee-Challenges-Gaara-In-Naruto-768x384.jpg.webp",
+    "title": "Rock lee se supera na luta contra o gaara!",
+    "description": "Tomou uma surra mas lutou."
+  },
+  {
+    "id":3,
+    "photo":"https://live.staticflickr.com/1045/1259361191_e8eb1bf49e_z.jpg",
+    "title": "Briga de familía acaba com intervenção!",
+    "description": "Num superei o neji morreu pra um pedaço de pau."
+  },
+  {
+    "id":4,
+    "photo":"https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2022/03/legiao_Jix_YVFdDKXr.jpg.webp",
+    "title": "Uchiha relembra konoha porque foram de F!",
+    "description": "Já diria Tobirama, Uchiha bom é uchiha morto!"
+  }
+
+]
